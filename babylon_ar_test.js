59bd0788001b08e4872b232eafb64f8dc18256c1
Ashish-Cryenx <84885970+Ashish-Cryenx@users.noreply.github.com>
2021-07-28 14:57:53 +0530

    Update babylon_ar_test.js

--- babylon_ar_test.js
+++ babylon_ar_test.js
@@ -8,6 +8,30 @@ window.addEventListener('DOMContentLoaded', function(){
     var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true }); };
     
     
+    
+        var loadingScreenDiv = window.document.getElementById("loadingScreen");
+
+        function customLoadingScreen() {
+            console.log("customLoadingScreen creation")
+        }
+    
+    customLoadingScreen.prototype.displayLoadingUI = function () {
+            console.log("customLoadingScreen loading")
+            loadingScreenDiv.innerHTML = "loading";
+        };
+        customLoadingScreen.prototype.hideLoadingUI = function () {
+            console.log("customLoadingScreen loaded")
+            loadingScreenDiv.style.display = "none";
+        };
+        var loadingScreen = new customLoadingScreen();
+        engine.loadingScreen = loadingScreen;
+
+        engine.displayLoadingUI();
+
+    
+    
+    
+    
     var createScene = async function () {
         var pushButtonCore;
         var index = 0; 
@@ -32,6 +56,29 @@ window.addEventListener('DOMContentLoaded', function(){
                 chair = mesh[0].getChildTransformNodes(false)[0];
                mesh.rotation = new BABYLON.Vector3(0, 180, 0);
                 chair.isVisible = false;
+            
+            function () {
+        // onSuccess
+        scene.createDefaultCamera(true, true, true);
+        scene.activeCamera.alpha = Math.PI / 2;
+        engine.hideLoadingUI();
+    },
+    function (evt) {
+        // onProgress
+        var loadedPercent = 0;
+        if (evt.lengthComputable) {
+            loadedPercent = (evt.loaded * 100 / evt.total).toFixed();
+        } else {
+            var dlCount = evt.loaded / (1024 * 1024);
+            loadedPercent = Math.floor(dlCount * 100.0) / 100.0;
+        }
+        // assuming "loadingScreenPercent" is an existing html element
+        document.getElementById("loadingScreenPercent").innerHTML = loadedPercent;
+    }
+            
+            
+            
+            
         });
     
         // var xr = await scene.createDefaultXRExperienceAsync({floorMeshes: []})
