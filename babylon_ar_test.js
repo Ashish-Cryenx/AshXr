4dbe4c7536c7e01d412851c83347dd5517183712
Ashish-Cryenx <84885970+Ashish-Cryenx@users.noreply.github.com>
2021-08-09 15:12:00 +0530

    Update babylon_ar_test.js

--- babylon_ar_test.js
+++ babylon_ar_test.js
@@ -17,6 +17,7 @@ window.addEventListener('DOMContentLoaded', function(){
     
         var scene = new BABYLON.Scene(engine);
           scene.clearColor = BABYLON.Color3.White();
+        scene.createDefaultEnvironment();
         var camera = new BABYLON.ArcRotateCamera("cam", Math.PI/2, Math.PI / 2, 3, new BABYLON.Vector3(0,0.5,0));
         var light = new BABYLON.HemisphericLight("sun", new BABYLON.Vector3(0,1,0), scene);
         var anchor = new BABYLON.TransformNode("");
