5864a25ea7b9953291fadd4389ecf64ad14cf660
Ashish-Cryenx <84885970+Ashish-Cryenx@users.noreply.github.com>
2021-08-09 20:03:22 +0530

    Update babylon_ar_test.js

--- babylon_ar_test.js
+++ babylon_ar_test.js
@@ -1,4 +1,36 @@
 // 360 Image logic
+BABYLON.DefaultLoadingScreen.prototype.displayLoadingUI = function () {
+    if (document.getElementById("customLoadingScreenDiv")) {
+        // Do not add a loading screen if there is already one
+        document.getElementById("customLoadingScreenDiv").style.display = "initial";
+        return;
+    }
+    this._loadingDiv = document.createElement("div");
+    this._loadingDiv.id = "customLoadingScreenDiv";
+    this._loadingDiv.innerHTML = "scene is currently loading";
+    var customLoadingScreenCss = document.createElement('style');
+    customLoadingScreenCss.type = 'text/css';
+    customLoadingScreenCss.innerHTML = `
+    #customLoadingScreenDiv{
+        background-color: #BB464Bcc;
+        color: white;
+        font-size:50px;
+        text-align:center;
+    }
+    `;
+    document.getElementsByTagName('head')[0].appendChild(customLoadingScreenCss);
+    this._resizeLoadingUI();
+    window.addEventListener("resize", this._resizeLoadingUI);
+    document.body.appendChild(this._loadingDiv);
+};
+
+BABYLON.DefaultLoadingScreen.prototype.hideLoadingUI = function(){
+    document.getElementById("customLoadingScreenDiv").style.display = "none";
+    console.log("scene is now loaded");
+}
+
+
+
 window.addEventListener('DOMContentLoaded', function(){
     var canvas = document.getElementById("renderCanvas");
 
@@ -9,6 +41,7 @@ window.addEventListener('DOMContentLoaded', function(){
     
     
     var createScene = async function () {
+         engine.displayLoadingUI();
         var pushButtonCore;
         var index = 0; 
     
